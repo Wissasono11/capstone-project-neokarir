6eb0fda117d9064e235d0710a7c96be7b98dcf91
Wissasono11 <bayuwicaksono782@gmail.com>
2026-06-03 23:20:24 +0700

    fix: bug at chatbots history

--- backend/src/repositories/chat.repository.js
+++ backend/src/repositories/chat.repository.js
@@ -29,7 +29,7 @@ const create = async (userId, payload, accessToken) => {
 };
 
 const isUuid = (str) => {
-  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
   return typeof str === 'string' && uuidRegex.test(str);
 };
 
